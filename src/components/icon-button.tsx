9d02ffc4888f265993e9e033e5f9b2df3cde8feb
Luiza Sofal <lrsofal@sga.pucminas.br>
2025-02-21 14:09:49 -0300

    Aula 1 finalizada

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -5,7 +5,8 @@ interface ButtonProps extends ComponentProps<'button'> {}
 export function IconButton(props: ButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors 
+      duration-300 hover:bg-blue hover:text-gray-900"
       {...props}
     />
   )
